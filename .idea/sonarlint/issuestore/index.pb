e51cff5443ac6e017def3902bafa9bd4b83cfb6e
Chelsea Campbell <cpcampbe@calpoly.edu>
2021-10-21 00:52:46 -0700

    added run function in classify.java, line 118 is still messed

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,19 @@
+
+A
+adult-stretch.csv,e\4\e4a6a2edffeeff1151ff62c2dff26ece88ce0ca1
+>
+openHouses.csv,1\b\1bd7a18cce87a61d64ef0f4083c608a3d3f777e6
+A
+nursery.names.txt,e\1\e18a39405c21384c4a493e1589d6e84ed32968e1
+;
+nursery.csv,2\e\2e54e3c8f15e883916f54a642edbeb4db9be35a5
+?
+openHouses.json,d\f\df2cd71e35406fd25046003be34408233840e2e0
+?
+sampleTree.json,a\0\a09e6ee9620293a705f4152ce913f6dab24ebd1a
+>
+InduceC45.java,b\b\bbc35dc5fe88b04a5883ed91a7adea0c17839610
+=
+Classify.java,7\7\77e2a0ab9b7abfc47a21f50c23ac6cbe13a8b0c9
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- Classify.java
+++ Classify.java
@@ -1,9 +1,11 @@
 import java.io.FileReader;
 import java.util.*;
-import java.util.Map.*;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.*;
+
+import org.json.JSONException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.*;
 
 class Classify{
@@ -11,6 +13,52 @@ class Classify{
     // args[0]: csv file
     // args[1]: json file
     public static void main(String[] args) throws Exception {
+        // parsing json file and typecasting to JSONObject
+        JSONObject obj = (JSONObject) new JSONParser().parse(new FileReader(args[1]));
+        JSONObject node1 = (JSONObject) obj.get("node");
+        Node n = new Node((String)node1.get("var"), "", (ArrayList<Edge>)node1.get("edges"), -1);
+        ConfusionMatrix matrix = new ConfusionMatrix();
+        ArrayList<String> arr = new ArrayList<>();
+
+        // parsing csv file
+        List<String> colNames = new ArrayList<>();
+        CategoryAttr cat = null;
+        String line = "";
+        String splitBy = ",";
+        try {
+            //parsing a CSV file into BufferedReader class constructor
+            BufferedReader br = new BufferedReader(new FileReader(args[0]));
+            int currentLine = 0;
+            while ((line = br.readLine()) != null)   //returns a Boolean value
+            {
+                String[] entry = line.split(splitBy);    // use comma as separator
+                if (currentLine == 0)
+                    colNames = Arrays.asList(entry);
+                else if (currentLine == 1) {
+                    currentLine++;
+                    continue;
+                }
+                else if (currentLine == 2 && !entry[0].isEmpty())
+                    cat = new CategoryAttr(entry[0], getColIndex(entry[0], colNames));
+                else {
+                    getPrediction(n, entry, colNames, cat, matrix, arr);
+                }
+                currentLine++;
+            }
+            System.out.println("Total Number Of Records Classified: " + matrix.total);
+            System.out.println("Total Number Of Records Correctly Classified: " + matrix.corrCount);
+            System.out.println("Total Number Of Records Incorrectly Classified: " + matrix.incorrCount);
+            System.out.println("Overall Accuracy of the Classifier: " + matrix.corrCount/matrix.total);
+            System.out.println("Overall Error Rate of the Classifier: " + (1-(matrix.corrCount/matrix.total)));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> run(String[] args) throws Exception {
+        ArrayList<String> classifiers = new ArrayList<>();
+
         // parsing json file and typecasting to JSONObject
         JSONObject obj = (JSONObject) new JSONParser().parse(new FileReader(args[1]));
         JSONObject node1 = (JSONObject) obj.get("node");
@@ -37,9 +85,8 @@ class Classify{
                 }
                 else if (currentLine == 2 && !entry[0].isEmpty())
                     cat = new CategoryAttr(entry[0], getColIndex(entry[0], colNames));
-                else {
-                    getPrediction(n, entry, colNames, cat, matrix);
-                }
+                else
+                    getPrediction(n, entry, colNames, cat, matrix, classifiers);
                 currentLine++;
             }
             System.out.println("Total Number Of Records Classified: " + matrix.total);
@@ -51,6 +98,7 @@ class Classify{
         catch (IOException e) {
             e.printStackTrace();
         }
+        return classifiers;
     }
 
     public static int getColIndex(String colName, List<String> colNames) {
@@ -61,14 +109,15 @@ class Classify{
         return -1;
     }
 
-    public static void getPrediction(Node n, String[] entry, List<String> colNames, CategoryAttr cat, ConfusionMatrix m){
+    public static void getPrediction(Node n, String[] entry, List<String> colNames, CategoryAttr cat, ConfusionMatrix m, ArrayList<String> arr) throws JSONException {
         boolean isLeaf = false;
         while(isLeaf == false) {
             int startIndex = getColIndex(n.attribute, colNames);
             String edgeName = entry[startIndex];
-            for (int i=0; i<n.edges.toArray().length; i++) {
-                if (edgeName.equals(n.edges.get(i).edge)) { // if we are on correct node
+            for (int i=0; i<n.edges.size(); i++) {
+                if (edgeName.equals(n.edges.get(i).toJSON().getJSONObject("edge").getString("value"))) { // if we are on correct node
                     if (!n.edges.get(i).next.decision.isEmpty()) { // if it is a leaf node
+                        arr.add(n.edges.get(i).next.attribute);
                         if (!cat.name.isEmpty()) { // if it has category attribute
                             String decision = n.edges.get(i).next.decision;
                             if (entry[cat.col] == decision)
